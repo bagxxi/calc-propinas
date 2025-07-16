6801ed5efd546ac98e8c115a604aef730a516c9e
SoloLeveling-dev25 <edutecnosabina@gmail.com>
2025-07-16 09:27:48 -0400

    Calculadora propina con Retos adicionales

---
+++ app/src/main/java/com/sakhura/calculadorapropinas/utils/CalculadoraUtils.kt
@@ -0,0 +1,9 @@
+package com.sakhura.calculadorapropinas.utils
+
+
+
+object CalculadoraUtils {
+    fun calcularPropina(monto: Double, porcentaje: Double): Double {
+        return monto * porcentaje
+    }
+}
